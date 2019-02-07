8829d488f86f8e946f83ad957c29a8421c539a52
= <=>
2019-02-07 11:12:15 +0530

    update

--- frac.py
+++ frac.py
@@ -7,4 +7,9 @@ def add(num1,den1,num2,den):
 def main():
     num,den=add(1,2,1,3)
     print(num,den)
-    '''print("%d/%d + %d/%d is %d/%d"%(1,2,1,3,num,den))'''
+    '''print("%d/%d + %d/%d is %d/%d"%(1,2,1,3,num,den))'''      
+    
+def addfractions(f1,f2):
+    denominator=f1[1]*f2[1]
+    numerator=(f1[0]*f2[1])+(f2[0]*f1[1])
+    return(numerator,denomimator)
