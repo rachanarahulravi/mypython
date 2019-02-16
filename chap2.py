8e3a0d652f67656c31fbe47328fd3cb759247621
= <=>
2019-02-16 12:19:44 +0530

    commit

---
+++ chap2.py
@@ -0,0 +1,18 @@
+
+#exercise 2
+username=input('enter your name:')
+print("hello"+username)
+
+
+#exercise 3
+hours=('enter hours:')
+rate=('enter rate:')
+pay=float(hours)*float(rate)
+print('gross pay is:'+pay)
+
+
+#exercise 5
+temp_celcius=('enter temperature in celcius:')
+temp_fareinheight=(float(temp_celcius)*1.8)+32
+print('temperature is:'+ temp_farenheight)
+
