a23aaa807d92d14cd20a0cb04741101ab5688dbb
= <=>
2019-02-20 18:40:47 +0530

    commit

---
+++ compare.py
@@ -0,0 +1,23 @@
+
+def compare(a,b,c):
+    if(a>=b) and (a>=c):
+        largest=a
+    elif(b>=a) and (b>=c):
+        largest=b
+    else:
+        largest=c
+    return largest
+
+
+def compare(a,b,c):
+    if a>b:
+        if a>c:
+            largest=a
+        else:
+            largest=c
+    else:
+        if b>c:
+            largest=b
+        else:
+            largesr=c
+    return largest
