d0b1c75629c0d6f1db602976108192d100d5b409
= <=>
2019-02-26 10:05:01 +0530

    commit

--- compare.py
+++ compare.py
@@ -1,8 +1,8 @@
 
 def compare(a,b,c):
-    if(a>=b) and (a>=c):
+    if(a>b) and (a>c):
         largest=a
-    elif(b>=a) and (b>=c):
+    elif(b>a) and (b>c):
         largest=b
     else:
         largest=c
